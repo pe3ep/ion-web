2c189457d61b52eebcad6d836ba5f3fbe6863505
Andrew <legendarysuwe@gmail.com>
2022-05-14 16:27:27 +0300

    🌓 New dark background color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,23 +20,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid h-screen place-content-center">
-        <div className="flex flex-col items-center gap-8">
-          <h1 className="heading text-6xl">
-            Current theme is:{' '}
-            <span className="text-blue-600 dark:text-blue-500">{theme}</span>
-          </h1>
-
-          <div className="flex gap-4">
-            <button className="button" onClick={() => setTheme('dark')}>
-              Set Dark theme
-            </button>
-            <button className="button-accent" onClick={() => setTheme('light')}>
-              Set Light theme
-            </button>
-          </div>
-        </div>
-      </div>
+      <header></header>
     </>
   )
 }
